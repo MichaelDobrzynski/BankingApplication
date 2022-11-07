83ef420b5f50ef0c9b52b52b96e99cd6e09ca49c
midoit01 <midoit01@hs-esslingen.de>
2022-11-07 14:45:06 +0100

    generate base of account number, still need to check whether savings or checking

--- src/Account.java
+++ src/Account.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 //abstract class, therefore won't be able to create objects from Account class, need to specify type of account
 public abstract class Account implements BaseInterestRate {
 
@@ -5,8 +7,11 @@ public abstract class Account implements BaseInterestRate {
     private String name;
     private String ssn;
     private Double balance;
-    private int accountNumber;
+    private String accountNumber;
     private double rate;
+
+    //index to help with unique 5-digit numbers used for accounts
+    static int index = 10000;
 
     //Constructor to set base properties and initialize account
     public Account(String name, String ssn, double initDeposit) {
@@ -17,7 +22,21 @@ public abstract class Account implements BaseInterestRate {
         System.out.println("Name: " + name);
         System.out.println("SSN: " + ssn);
         System.out.println("Initial deposit: $" + initDeposit);
+
+        //set account number
+        index++;
+        this.accountNumber = setAccountNumber();
+        System.out.println("Account Number: " + accountNumber);
+
         System.out.print("NEW ACCOUNT: ");
+    }
+
+    private String setAccountNumber() {
+        String lastTwoSsn = ssn.substring(ssn.length() - 2, ssn.length());
+        int uniqueFiveDigits = index;
+        int randomNumber = (int) (Math.random() * Math.pow(10, 3));
+
+        return lastTwoSsn + uniqueFiveDigits + randomNumber;
     }
 
     //common methods
